334b7244fb2a2d855801643405dfe460d5d8d1c8
0019 <1049092041@qq.com>
2020-12-31 16:24:32 +0800

    初始化

---
+++ service-hi/src/main/java/com/example/servicehi/ServiceHiApplication.java
@@ -0,0 +1,21 @@
+package com.example.servicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceHiApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+
+}
